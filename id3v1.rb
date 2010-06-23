ab1a182a971d952400c150e62b297ebf8e5920c7
Reese Moore <ram@vt.edu>
2010-06-22 22:59:53 -0400

    More formatting.

--- id3v1.rb
+++ id3v1.rb
@@ -38,10 +38,10 @@ class ID3v1
 		# blank.
 		@tag = data[0, 3]
 		if @tag == "TAG"
-			@title   = data[3, 30].strip
+			@title   = data[3,  30].strip
 			@artist  = data[33, 30].strip
 			@album   = data[63, 30].strip
-			@year    = data[93, 4].strip
+			@year    = data[93,  4].strip
 			@comment = data[97, 30].strip
 			@genre   = data[127, 1].strip
 		else
